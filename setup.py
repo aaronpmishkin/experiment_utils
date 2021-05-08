631348d92dc0a7e6e7bb661f5ad1010c55ff148d
Aaron Mishkin <aaronpmishkin@gmail.com>
2021-05-07 19:14:06 -0700

    Rollback python version.

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
     package_dir={"": "src"},
     package_data={"experiment_utils": ["py.typed"]},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.6",
 )
 
