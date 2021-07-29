fcce4fb7e88d870dfa0198d1188969b2cb2f6a19
Aaron Mishkin <aaronpmishkin@gmail.com>
2021-07-29 11:20:29 -0700

    Add stars to plot cell.

--- src/experiment_utils/plotting/plot_cell.py
+++ src/experiment_utils/plotting/plot_cell.py
@@ -52,6 +52,7 @@ def make_convergence_plot(
             x = np.arange(len(y))
 
         ax.plot(x, y, alpha=settings["line_alpha"], **line_kwargs[line])
+        ax.plot([x[-1]], [y[-1]], color=line_kwargs[line]["c"], marker="*", markersize=18)
 
 
 def make_error_bar_plot(
