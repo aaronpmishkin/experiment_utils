5c2ee9cd1ac4dc07c7c533157798f36d60a410cf
Aaron Mishkin <aaronpmishkin@gmail.com>
2021-09-22 22:19:25 -0700

    Update utils.

--- src/experiment_utils/utils.py
+++ src/experiment_utils/utils.py
@@ -1,6 +1,7 @@
 """
 Generic utilities.
 """
+from copy import deepcopy
 from typing import List, Any, Tuple, Union, Dict, Callable, Optional
 from functools import reduce, partial
 from warnings import warn
@@ -198,6 +199,36 @@ def cum_sum(filter_func: Callable):
         return vals
 
     return closure
+
+
+# data transformation functions
+
+
+def replace_x_axis(metric_grid):
+    results_grid = deepcopy(metric_grid)
+
+    for row in metric_grid.keys():
+        for metric_name in metric_grid[row].keys():
+            for line in metric_grid[row][metric_name].keys():
+                results = {}
+                repeats = metric_grid[row][metric_name][line].keys()
+
+                for (x_key, repeat_key) in repeats:
+                    vals = results_grid[row][metric_name][line][(x_key, repeat_key)]
+
+                    results[repeat_key] = results.get(repeat_key, []) + [
+                        (x_key, vals[-1])
+                    ]
+
+                # make sure the order is correct.
+                for key, val in results.items():
+                    results[key] = np.array(
+                        [final for lam, final in sorted(val, key=lambda x: x[0])]
+                    )
+
+                results_grid[row][metric_name][line] = results
+
+    return results_grid
 
 
 # Logging #
